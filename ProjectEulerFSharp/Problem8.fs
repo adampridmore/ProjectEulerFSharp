e716f1c414b191f02e3f48633cc6fb06f670db2b
Adam Pridmore <adam_pridmore@hotmail.com>
2015-04-12 17:21:26 +0100

    - Solved problem 8.

---
+++ ProjectEulerFSharp/Problem8.fs
@@ -0,0 +1,60 @@
+﻿module Problem8
+
+open primes
+open NUnit.Framework
+open FsUnit
+
+// The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
+//
+// Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
+
+let numberCharacters = 
+  let numberLines = [ "73167176531330624919225119674426574742355349194934";
+                      "96983520312774506326239578318016984801869478851843";
+                      "85861560789112949495459501737958331952853208805511";
+                      "12540698747158523863050715693290963295227443043557";
+                      "66896648950445244523161731856403098711121722383113";
+                      "62229893423380308135336276614282806444486645238749";
+                      "30358907296290491560440772390713810515859307960866";
+                      "70172427121883998797908792274921901699720888093776";
+                      "65727333001053367881220235421809751254540594752243";
+                      "52584907711670556013604839586446706324415722155397";
+                      "53697817977846174064955149290862569321978468622482";
+                      "83972241375657056057490261407972968652414535100474";
+                      "82166370484403199890008895243450658541227588666881";
+                      "16427171479924442928230863465674813919123162824586";
+                      "17866458359124566529476545682848912883142607690042";
+                      "24219022671055626321111109370544217506941658960408";
+                      "07198403850962455444362981230987879927244284909188";
+                      "84580156166097919133875499200524063689912560717606";
+                      "05886116467109405077541002256983155200055935729725";
+                      "71636269561882670428252483600823257530420752963450"
+                     ]
+  let numberText = numberLines |> List.reduce (+)
+  numberText.ToCharArray() |> Array.toList |> List.map (fun c -> System.Int32.Parse(c.ToString()))
+
+let solver numbersToTake =
+  let getNumbersFromBigNumber startIndex length =
+    numberCharacters |> Seq.skip startIndex |> Seq.take length
+
+  let productOf seq =
+    seq |> Seq.map (fun i -> int64 i) |> Seq.reduce (*)
+
+  seq{0..numberCharacters.Length - numbersToTake} 
+    |> Seq.map (fun i -> (getNumbersFromBigNumber i numbersToTake) |> productOf)
+    |> Seq.max
+
+let problem8 = 
+ solver 13
+
+[<Test>]
+let ``answer``() =
+  let ans = problem8
+  printfn "%i" ans
+  ans |> should equal 23514624000L
+
+[<Test>]
+let ``longest 4 number product is 5832``() =
+  let ans = solver 4
+  ans |> should equal 5832
+    
--- ProjectEulerFSharp/Program.fs
+++ ProjectEulerFSharp/Program.fs
@@ -8,6 +8,7 @@ open Problem4
 open Problem5
 open Problem6
 open Problem7
+open Problem8
 
 [<EntryPoint>]
 let main argv = 
@@ -18,5 +19,6 @@ let main argv =
   printfn "Problem 5 = %i" problem5
   printfn "Problem 6 = %i" problem6
   printfn "Problem 7 = %i" problem7
-  
+  printfn "Problem 8 = %i" problem8
+
   0 // return an integer exit code
