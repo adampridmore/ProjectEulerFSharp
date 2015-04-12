82562b47eb26cb8429b32737fe7f53cb635ae4d8
Adam Pridmore <adam_pridmore@hotmail.com>
2015-04-12 11:41:08 +0100

    - Initial version of problem 4.

--- ProjectEulerFSharp/Problem4.fs
+++ ProjectEulerFSharp/Problem4.fs
@@ -7,9 +7,35 @@ open FsUnit
 //
 //Find the largest palindrome made from the product of two 3-digit numbers.
 
+let isPalindrome i =
+  let numberCharacters = i.ToString().ToCharArray()
+  let reversedCharacters = Array.rev(numberCharacters)
+  numberCharacters = reversedCharacters
+
 let problem4 = 
-  0
+  let maxNum = 999
+  let nums = (Seq.unfold (fun state -> 
+      match state with 
+      | 0,0 -> None
+      | 0,y -> Some((maxNum * y),(maxNum, y-1))
+      | x,y -> Some((x * y),(x - 1, y))
+    ) (maxNum, maxNum)
+  )
+
+  nums 
+  |> Seq.filter (fun i -> isPalindrome i)
+  |> Seq.max 
 
 [<Test>]
-let ``scratch``() =
-  printfn "Test"
+let ``answer``() =
+  let ans = problem4
+  printfn "%i" ans
+  ans |> should equal 906609
+
+[<Test>]
+let ``123 is not a palindrome``() =
+  isPalindrome 123 |> should equal false
+
+[<Test>]
+let ``1221 is a palindrome``() =
+  isPalindrome 1221 |> should equal true
