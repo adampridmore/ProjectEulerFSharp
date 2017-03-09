f066af077cc72b3810d8d55d75293a217e96d5e9
Adam Pridmore <adam_pridmore@hotmail.com>
2017-03-09 20:37:22 +0000

    - Added permutations module.

---
+++ MyFSharpHelpers/Permutations.fs
@@ -0,0 +1,57 @@
+﻿module Permutations
+
+open Microsoft.FSharp.Collections
+
+let rec insertions pre c post =
+    seq {
+        if List.length post = 0 then
+            yield pre @ [c]
+        else
+            if List.forall (fun x->x<>c) post then
+                yield pre@[c]@post
+            yield! insertions (pre@[post.Head]) c post.Tail
+        }
+
+let rec permutations l =
+    seq {
+        if List.length l = 1 then
+            yield l
+        else
+            let subperms = permutations l.Tail
+            for sub in subperms do
+                yield! insertions [] l.Head sub
+        }
+
+let getAllPerms = permutations
+
+let numberOfPermutations pipes = 
+    let tobigint (x:int) =
+        bigint x
+
+    let rec fact x = 
+        match x with
+        | x when x = bigint.One -> bigint.One
+        | x -> x * fact (x - bigint.One)
+
+    let permutations = fact (pipes |> Seq.length |> bigint) 
+    let dupesFactor =   pipes 
+                        |> Seq.groupBy id
+                        |> Seq.map (fun (_,x) -> x |> Seq.length |> tobigint |> fact)
+                        |> Seq.reduce (*)
+
+    permutations / dupesFactor
+
+
+
+
+let PGetAllPerms pipes = 
+    let getOtherPerms pipe (otherPipes: int list ) =
+        otherPipes 
+        |> getAllPerms
+        |> Seq.map (fun perm -> List.concat [[pipe];perm] )
+
+    pipes
+    |> PSeq.map (fun pipe -> pipes
+                             |> List.filter (fun p -> p <> pipe)
+                             |> (fun otherPipes -> otherPipes |> getOtherPerms pipe))
+    |> PSeq.collect id
---
+++ MyFSharpHelpers/PermutationsTests.fs
@@ -0,0 +1,40 @@
+﻿module PermutationsTests
+
+open NUnit.Framework
+open FsUnit
+open Permutations
+
+[<Test>]
+let ``get all permutations``() = 
+    [1;2;3]
+    |> getAllPerms
+    |> Seq.map (fun x -> x |> printfn "%A"; x)
+    |> should equal [
+        [1;2;3]
+        [2;1;3]
+        [2;3;1]
+        [1;3;2]
+        [3;1;2]
+        [3;2;1]
+        ]
+
+
+[<Test>]
+let ``get all permutations with dup values``() = 
+    [1;1;2]
+    |> getAllPerms
+    |> Seq.map (fun x -> x |> printfn "%A"; x)
+    |> should equal [
+        [1;1;2]
+        [1;2;1]
+        [2;1;1]
+        ]
+
+//[<Test>]
+//let ``get lots of permutations``() = 
+//
+//// TODO - Add stopwatch
+//    [1;1;2;2;3;3;4;4;5;5]
+//    |> getAllPerms
+//    |> Seq.truncate 1000
+//    ()
