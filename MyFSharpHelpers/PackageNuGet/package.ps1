fa0fdc7012b718c9eb3740428230fef209e75fbc
Adam Pridmore <adam_pridmore@hotmail.com>
2017-03-09 20:13:21 +0000

    - Created new NugetPack 1.0.0.3

--- MyFSharpHelpers/PackageNuGet/package.ps1
+++ MyFSharpHelpers/PackageNuGet/package.ps1
@@ -3,3 +3,5 @@ nuget pack ..\MyFSharpHelpers.fsproj
 
 # This doesn't seem to like relative paths.
 nuget push *.nupkg -source "C:\Users\Adam\Dropbox\Work\Dev\NugetPackages\MyFSharpHelpers"
+
+del *.nupkg
