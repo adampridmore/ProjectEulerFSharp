244cafcaa7851e7a79f090cb2444a6852bd1954b
Adam Pridmore <adam_pridmore@hotmail.com>
2018-01-27 00:16:59 +0000

    - Added a plane solution to problem 1.

--- ProjectEulerFSharpPlain/Problem1.fsx
+++ ProjectEulerFSharpPlain/Problem1.fsx
@@ -5,3 +5,18 @@
 //
 //Find the sum of all the multiples of 3 or 5 below 1000.
 
+let (|Divisible|_|) x = if x % 3 = 0 || x % 5 = 0 then Some() else None 
+let (|InRange|_|) x = if x < 1000 then Some() else None
+
+let rec fn x max =
+    match x with
+    | InRange -> 
+        let newTotal = 
+            match x with
+            | Divisible -> x
+            | _ -> 0
+        newTotal + fn (x+1) max
+    | _ -> 0
+
+fn 1 1000
+//233168
